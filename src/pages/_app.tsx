d092827f1168ce41ee99cb1246721a188de5fef1
Cooper Reed <cooperability@gmail.com>
2025-12-20 12:59:11 -0800

    feat: fix bad prop passed to Vercel SpeedInsights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import type { NextPage } from 'next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import { useRouter } from 'next/router'
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: React.ReactElement) => React.ReactNode
@@ -16,14 +15,12 @@ type AppPropsWithLayout = AppProps & {
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
-  const router = useRouter()
 
   return (
     <ThemeProvider enableSystem={true} attribute="class">
       {getLayout(<Component {...pageProps} />)}
       <Analytics />
-      {/* Route prop helps with accurate per-page metrics */}
-      <SpeedInsights route={router.pathname} />
+      <SpeedInsights />
       <script
         dangerouslySetInnerHTML={{
           __html:
